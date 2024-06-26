d08765426dc1b3267a3b8fca1517bdc85860078a
Stronauta <samircastillop@gmail.com>
2024-06-26 00:18:13 -0400

    Add project files.

---
+++ DAL/AnimalsContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiAnimals.Models;
+
+namespace WebApiAnimals.DAL
+{
+    public class AnimalsContext: DbContext
+    {
+        public AnimalsContext(DbContextOptions<AnimalsContext> options) : base (options)
+        {
+            
+        }
+
+        public DbSet<Animal> Animals { get; set; }
+    }
+}
---
+++ Models/Animal.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApiAnimals.Models
+{
+    public class Animal
+    {
+        public int AnimalId { get; set; }
+        public string Name { get; set; }
+        public string Specie { get; set; }
+        public int Age { get; set; }
+        public double Weight { get; set; }
+
+    }
+}
